4c6e4033f7161eb36789b466a68f08957d8dbbd2
TeeJude <teejayjude@gmail.com>
2024-01-11 21:47:03 +0000

    lockboxes

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-   A function that determines if all boxes 
-   can be opened.
+"""Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists
 """
 
 
