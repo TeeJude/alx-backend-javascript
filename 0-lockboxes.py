8398b0485ea13d0fca628e7c74b5beec43591c0e
TeeJude <teejayjude@gmail.com>
2024-01-11 21:44:02 +0000

    lockboxes

---
+++ 0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+   A function that determines if all boxes 
+   can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """Determines if boxes can be unlocked"""
+    position = 0
+    unlocked = {}
+
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
